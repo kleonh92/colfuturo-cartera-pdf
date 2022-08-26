9865acb51e70bffa50cccf158a859cd566ae2ad0
Jackeline Hernandez <jhernandez@serempre.com>
2022-08-26 14:21:16 -0500

    .

---
+++ resources/views/excel/sheet1.blade.php
@@ -0,0 +1,45 @@
+
+
+
+        <table class="header">
+            <tr>
+                <td class="header_logo" width="10%">
+{{--                    <img style="width:200px; height:75px;" src="" alt="Logo" />--}}
+
+                </td>
+                <td></td>
+                <td width="35%">
+                    <small class="font-roboto font-medium text-color-primary text-base">Paloma Geraldine David Velásquez de la Santísima
+                        Trinidad</small>
+                </td>
+            </tr>
+            <tr>
+                <td></td><td></td>
+                <td width="35%">
+                    <small class="font-roboto font-normal text-sm text-neutral-90">Maestría/Teacher Education and Learning Sciences of Social Arts
+                        and Europe</small>
+                </td>
+            </tr>
+            <tr>
+                <td></td><td></td>
+                <td width="35%">
+                    <small class="font-roboto font-normal text-sm text-neutral-90">Universiteit van Amsterdam - Uva Amsterdam, Noord-Holland /
+                        Holanda</small>
+                </td>
+            </tr>
+                <td width="35%">
+                    <small class="font-roboto font-normal text-sm text-neutral-80">CC: <span class="font-medium text-neutral-90">1.018.473.865 </span>
+                    </small>
+                </td>
+                <td>
+                    <small class="font-roboto font-normal text-sm text-neutral-80">Código: <span class="font-medium text-neutral-90">201.806.121
+                        </span></small>
+                </td>
+                <td>
+                    <small class="font-roboto font-normal text-sm text-neutral-80">Estado: <span class="font-medium text-neutral-90">PAEC(Periodo Amortización
+                                    Exterior con
+                                    Condonación)</span></small>
+                </td>
+            </tr>
+        </table>
+
